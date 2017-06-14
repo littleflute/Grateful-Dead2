d214025d13103f5bc586b4150aa5e9fdf8d57e2f
littleflute <13699175164@sina.cn>
2017-06-14 15:00:09 -0500

    Add files via upload

---
+++ 30 Trips Around the Sun- The Definitive Live Story 1965-1995 Disc 4/m.js
@@ -0,0 +1,10 @@
+var f = [];				
+f[0]	= "v0.0.8";			
+f[1]	= "01 Not Fade Away [Live at Oxford Plains Speedway, Oxford].mp3";		
+f[2]	= "02 Blow Away [Live at Miami Arena, Miami].mp3";		
+f[3]	= "03 Ramble On Rose [Live at Zenith, Paris, France].mp3";		
+f[4]	= "04 High Time [Live From Madison Square Garden, New York].mp3";		
+f[5]	= "05 Althea [Live at Copps Coliseum, Ontario, Canada].mp3";		
+f[6]	= "06 Broken Arrow [Live at Knickerbocker Arena, Albany].mp3";		
+f[7]	= "07 So Many Roads [Live at Boston Garden, Boston].mp3";		
+f[8]	= "08 Visions of Johanna [Delta Center, Salt Lake City].mp3";		
