cf637f2874e09ac705c87843e43d663dbbdb1c87
littleflute <13699175164@sina.cn>
2017-06-14 15:02:18 -0500

    Add files via upload

---
+++ Veneta, Oregon 8-27-72 [180 Gram] Disc 4/m.js
@@ -0,0 +1,8 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Dark Star.mp3";		
+f[2]	= "02 El Paso.mp3";		
+f[3]	= "03 Sing Me Back Home.mp3";		
+f[4]	= "04 Sugar Magnolia.mp3";		
+f[5]	= "05 Casey Jones.mp3";		
+f[6]	= "06 One More Saturday Night.mp3";		
